837ebefa4088251625f5927668b639fcc4cec2df
Sidharth Mittal <Sidharth.Mittal@ibm.com>
2025-03-11 16:33:10 +0700

    modified upper servlet

--- modresorts-twas-j8/src/main/java/com/acme/modres/UpperServlet.java
+++ modresorts-twas-j8/src/main/java/com/acme/modres/UpperServlet.java
@@ -2,13 +2,11 @@ package com.acme.modres;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.io.BufferedInputStream;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
 import com.ibm.websphere.servlet.response.ResponseUtils;
 
